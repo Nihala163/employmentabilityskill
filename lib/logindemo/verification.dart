594d4417809712c03dce62804c5d5e89b299f01f
Nihalaferi163 <nihalaferi163@gmail.com>
2023-12-30 16:11:23 +0530

    first commit

--- lib/logindemo/verification.dart
+++ lib/logindemo/verification.dart
@@ -1,12 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kmoiti/logindemo/webview.dart';
 import 'package:pinput/pinput.dart';
 
 class Verification extends StatefulWidget {
-  final String phoneNumber;
+  final String verificationId;
 
-  const Verification({Key? key, required this.phoneNumber}) : super(key: key);
+  const Verification({Key? key, required this.verificationId}) : super(key: key);
 
   @override
   State<Verification> createState() => _VerificationState();
@@ -16,74 +17,7 @@ class _VerificationState extends State<Verification> {
   var otp = "";
   bool isLoading = false;
 
-  @override
-  void initState() {
-    super.initState();
-    // Initiate the auto-read verification process
-    _startPhoneAuth();
-  }
-
-  Future<void> _startPhoneAuth() async {
-    FirebaseAuth.instance.verifyPhoneNumber(
-      phoneNumber: widget.phoneNumber,
-      verificationCompleted: (PhoneAuthCredential credential) async {
-        // Sign in with the auto-received credential
-        await FirebaseAuth.instance.signInWithCredential(credential);
-
-        // Navigate to another page after successful verification
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context) => Home(), // Replace with your destination page
-          ),
-        );
-      },
-      verificationFailed: (FirebaseAuthException e) {
-        print("Error during auto-read OTP validation: ${e.message}");
-      },
-      codeSent: (String verificationId, int? resendToken) {
-        print("Code sent to ${widget.phoneNumber}");
-      },
-      codeAutoRetrievalTimeout: (String verificationId) {
-        print("Auto retrieval timeout. You can handle this if needed.");
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("OTP Verification"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Pinput(
-              keyboardType: TextInputType.number,
-              length: 6,
-              onChanged: (value) => otp = value,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: isLoading ? null : () => _verifyOtp(widget.phoneNumber),
-              child: Text('Verify OTP'),
-              style: ElevatedButton.styleFrom(
-                fixedSize: Size.fromWidth(200),
-                primary: isLoading ? Colors.grey : null,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Future<void> _verifyOtp(String phoneNumber) async {
+  Future<void> verifyOtp() async {
     try {
       setState(() {
         isLoading = true;
@@ -100,7 +34,7 @@ class _VerificationState extends State<Verification> {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(
-          builder: (context) => Home(), // Replace AnotherPage with your destination page
+          builder: (context) =>Home(), // Replace AnotherPage with your destination page
         ),
       );
     } catch (e) {
@@ -118,5 +52,40 @@ class _VerificationState extends State<Verification> {
         isLoading = false;
       });
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("OTP Verification"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Pinput(
+              keyboardType: TextInputType.number,
+              length: 6,
+              onChanged:(value) => otp = value,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: isLoading ? null : verifyOtp,
+              child: Text('Verify OTP'),
+              style: ElevatedButton.styleFrom(
+                fixedSize: Size.fromWidth(200),
+                // Disable the button while loading
+                primary: isLoading ? Colors.grey : null,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
